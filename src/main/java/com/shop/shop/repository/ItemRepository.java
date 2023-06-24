d94788ca0f3410a24bb8b4a6f159739dfa74da46
codesche <codesche@gmail.com>
2023-06-24 23:29:55 +0900

    feat_repository: repository @Query 어노테이션 활용한 테스트 코드 작성

--- src/main/java/com/shop/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/shop/repository/ItemRepository.java
@@ -3,6 +3,8 @@ package com.shop.shop.repository;
 import com.shop.shop.entity.Item;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
@@ -25,5 +27,18 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
      * OrderBy로 정렬하기
      */
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    /**
+     * @Query를 이용한 검색 처리
+     */
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    /**
+     * @Query-nativeQuery 속성을 활용
+     */
+    @Query(value = "select * from item i where i.item_detail like"
+        + " %:itemDetail% order by i.price desc", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
 
 }
