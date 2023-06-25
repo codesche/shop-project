eb3eae4bfe30c0c6ddc6b43f07a628b434c4566e
codesche <codesche@gmail.com>
2023-06-25 18:56:53 +0900

    feat_repository: querydsl을 활용한 상품 조회 테스트 코드 작성 2

--- src/main/java/com/shop/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/shop/repository/ItemRepository.java
@@ -4,9 +4,10 @@ import com.shop.shop.entity.Item;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
 
     /**
      * 상품 조회
